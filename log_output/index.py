f148638945e7bcbc1a779622f137796425a15093
Shuja Akbar <shujaakbar2020@gmail.com>
2025-11-21 21:13:04 +0500

    Completed exercise 1.7

--- log_output/index.py
+++ log_output/index.py
@@ -2,17 +2,19 @@ import time
 import uuid
 from datetime import datetime, timezone
 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
 def get_timestamp():
     # ISO 8601 with milliseconds and Z
     return datetime.now(timezone.utc).isoformat(timespec='milliseconds').replace("+00:00", "Z")
 
-def main():
+@app.route('/')
+def index():
     stored_string = str(uuid.uuid4())
-    print(f"{get_timestamp()}: {stored_string}")
-
-    while True:
-        time.sleep(5)
-        print(f"{get_timestamp()}: {stored_string}")
+    return jsonify({"message": f"{get_timestamp()}: {stored_string}"})
 
 if __name__ == "__main__":
-    main()
+    print("Server started in port 5000")   # printed on startup
+    app.run(host="0.0.0.0", port=5000)
