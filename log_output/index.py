b9c68ecfb1b6858e19a75677d4f2d064228f92a4
Shuja Akbar <shujaakbar2020@gmail.com>
2025-11-25 15:16:13 +0500

    Completed exercise 2.5

--- log_output/index.py
+++ log_output/index.py
@@ -1,4 +1,4 @@
-import time
+import time, os
 import uuid
 from datetime import datetime, timezone
 
@@ -15,9 +15,19 @@ def read_file():
     except FileNotFoundError:
         return f"Error: The file 'randoms.txt' was not found."
 
+def read_content_file():
+    try:
+        with open('information.txt', 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return f"Error: The file 'information.txt' was not found."
+
 @app.route('/')
 def index():
-    return jsonify({"message": read_file(), "pingpong": requests.get("http://pingpong-svc:5000/").text})
+    file_content = read_content_file()
+    return jsonify({"message": os.environ["MESSAGE"], "file_content": file_content, "pingpong": requests.get("http://pingpong-svc:5000/").text})
+
 if __name__ == "__main__":
     print("Server started in port 5000")   # printed on startup
     app.run(host="0.0.0.0", port=5000)
